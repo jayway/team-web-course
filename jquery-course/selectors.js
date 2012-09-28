2b3740c8de5ce6dc8a2de898f9ffda67a8f4ad11
Anders Janmyr <anders@janmyr.com>
2012-09-28 11:27:08 +0200

    Add one more child selector and a class selector

--- jquery-course/selectors.js
+++ jquery-course/selectors.js
@@ -2,23 +2,38 @@ $(function(exports) {
     // If you want to see what happens to the DOM
     // add a breakpoint in afterEach in the test.
 
-    // ID Selection, find the element with id markup and return it.
+    // ID Selection
+    // Find the element with id markup and return it.
     exports.findElementWithIdMarkup = function() {
         return $('#markup');
     };
 
-    // Sub selection, find the li elements below the markup id
+    // Sub selection
+    // Find the li elements below the markup id
     // and add class animal
     exports.findLisAndAddClassAnimal = function() {
         $('#markup li').addClass('animal');
     };
 
-    // Child selection, find the top level li elements below the markup id
+    // Find by class title below the markup id
+    // and remove the the class
+    exports.findByClassTitleAndRemoveIt = function() {
+        $('.title').removeClass('title');
+    };
+
+    // Child selection
+    // find the top level li elements below the markup id
     // and add class animal-group
     exports.findTopLevelLisAndAddClassAnimalGroup = function() {
         $('#markup > ul > li').addClass('animal-group');
     };
 
+    // Child selection
+    // find the top level li elements below the markup id
+    // and add class animal-group
+    exports.findChildLisAndAddClassAnimal= function() {
+        $('#markup > ul > li > ul > li').addClass('animal');
+    };
 
 }(window));
 
--- jquery-course/selectors_test.js
+++ jquery-course/selectors_test.js
@@ -10,22 +10,41 @@ describe('Selectors', function() {
         markup = $('#markup').html();
     });
 
-    it('Find the element with id="markup"', function() {
-        var actual = findElementWithIdMarkup();
-        expect(actual.attr('id')).to.equal('markup');
-        expect(actual.parent()[0].tagName).to.equal('BODY');
+    describe('Find by id', function() {
+        it('Find the element with id="markup"', function() {
+            var actual = findElementWithIdMarkup();
+            expect(actual.attr('id')).to.equal('markup');
+            expect(actual.parent()[0].tagName).to.equal('BODY');
+        });
     });
 
-    it("finds 11 li's with class animal", function() {
-        findLisAndAddClassAnimal();
-        var actual = $('li.animal');
-        expect(actual.length).to.equal(11);
+    describe('Sub selection, find by element', function() {
+        it("finds 11 li's with class animal", function() {
+            findLisAndAddClassAnimal();
+            var actual = $('li.animal');
+            expect(actual.length).to.equal(11);
+        });
     });
 
-    it("finds 2 top levels li's with class animal-group", function() {
-        findTopLevelLisAndAddClassAnimalGroup();
-        var actual = $('li.animal-group');
-        expect(actual.length).to.equal(2);
+    describe('Find by class', function() {
+        it("finds 2 h1 with class .title", function() {
+            findByClassTitleAndRemoveIt();
+            var actual = $('#markup h1:not(.title)');
+            expect(actual.length).to.equal(2);
+        });
+    });
+
+    describe('Child selection', function() {
+        it("finds 2 top level li's with class animal-group", function() {
+            findTopLevelLisAndAddClassAnimalGroup();
+            var actual = $('li.animal-group');
+            expect(actual.length).to.equal(2);
+        });
+        it("finds 9 child li's with class animal", function() {
+            findChildLisAndAddClassAnimal();
+            var actual = $('li.animal');
+            expect(actual.length).to.equal(9);
+        });
     });
 
     afterEach(function() {
