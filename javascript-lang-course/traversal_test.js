538f6ba24325424e23017dde0b478f0ac5075cc6
Peter Lindström <peter.lindstrom@jayway.com>
2012-09-28 13:59:50 +0200

    added mocha project

---
+++ javascript-lang-course/traversal_test.js
@@ -0,0 +1,22 @@
+describe('Traversal', function() {
+    var markup;
+    beforeEach(function() {
+        markup = $('#markup').html();
+    });
+
+    describe("Do something", function () {
+        beforeEach(function() {
+        });
+
+        it("Testing something", function() {
+        });
+
+
+        afterEach(function() {
+        });
+    });
+
+    afterEach(function() {
+        $('#markup').html(markup);
+    });
+});
