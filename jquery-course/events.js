fc03ba2ff7707f8ba0563d85a4a4a82c312b569c
Anders Janmyr <anders@janmyr.com>
2012-10-19 12:00:17 +0200

    Added tests for mouse and on

--- jquery-course/events.js
+++ jquery-course/events.js
@@ -2,13 +2,45 @@ $(function(exports) {
     // If you want to see what happens to the DOM
     // add a breakpoint in afterEach in the test.
 
-    // Adds class hightlight to #markup li on click
-    exports.addClickHandlerToLi = function() {
-        $('#markup li').click(function() {
+    // Adds class highlight to #markup button on click
+    // Make sure you prevent the default action by
+    // returning false from the callback or
+    // calling event.preventDefault
+    exports.addClickHandlerToButton = function() {
+        $('#markup button').click(function(event) {
+            event.preventDefault();
             $(this).addClass('highlight');
         });
     };
 
+
+    // Adds class highlight to #markup h1 on mouseover
+    // Remove class highlight to #markup h1 on mouseout
+    exports.addMouseOverAndMouseOutToH1 = function() {
+        $('#markup h1')
+            .mouseover(function() {
+                $(this).addClass('highlight');
+            })
+            .mouseout(function() {
+                $(this).removeClass('highlight');
+            });
+    };
+
+    // Adds class highlight to #markup li on click
+    // Use the on method instead of the click method
+    exports.addClickHandlerToLiWithOn = function() {
+        $('#markup li').on('click', function(event) {
+            $(this).addClass('highlight');
+        });
+    };
+
+    // Add a click handler on #markup ul
+    // The click handler should add class highlight to the li children
+    exports.addClickHandlerToUlForLiWithOn = function() {
+        $('#markup ul').on('click', 'li', function(event) {
+            $(this).addClass('highlight');
+        });
+    };
 
 
 }(window));
--- jquery-course/events_test.js
+++ jquery-course/events_test.js
@@ -6,14 +6,43 @@ describe('Events', function() {
 
     describe('click', function () {
         beforeEach(function() {
-            addClickHandlerToLi();
+            addClickHandlerToButton();
         });
 
+        it('highlights the button on click', function() {
+            $('#markup button').click();
+            expect($('#markup button')).to.have.class('highlight');
+        });
+    });
+
+    describe('mouse', function () {
+        beforeEach(function() {
+            addMouseOverAndMouseOutToH1();
+        });
+
+        it('highlights the h1 on over', function() {
+            $('#markup h1:first').mouseover();
+            expect($('#markup h1:first')).to.have.class('highlight');
+        });
+        it('removes highlight from h1 on out', function() {
+            $('#markup h1:first').mouseout();
+            expect($('#markup h1:first')).to.not.have.class('highlight');
+        });
+    });
+
+    describe('on', function () {
+
         it('highlights the li on click', function() {
+            addClickHandlerToLiWithOn();
             $('#markup li:first').click();
             expect($('#markup li:first')).to.have.class('highlight');
         });
 
+        it('highlights the li on click', function() {
+            addClickHandlerToUlForLiWithOn();
+            $('#markup li:first').click();
+            expect($('#markup li:first')).to.have.class('highlight');
+        });
     });
 
     afterEach(function() {
