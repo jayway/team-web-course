5b0e00d7f8f5e5c6d772679155a13d6310dc5019
Anders Janmyr <anders@janmyr.com>
2012-10-29 14:01:38 +0100

    Started to add unit tests

--- javascript-lang-course/string_calculator.js
+++ javascript-lang-course/string_calculator.js
@@ -1,6 +1,7 @@
 var jscourse = window.jscourse || {}
 jscourse.StringCalculator = StringCalculator = {
   add: function(inputString) {
+      return 1;
         // TODO
   }
 }
--- javascript-lang-course/string_calculator_spec.js
+++ javascript-lang-course/string_calculator_spec.js
@@ -5,28 +5,32 @@ describe("jscourse.StringCalculator", function() {
   describe("when an empty string is passed in", function() {
     it("returns 0", function() {
       var result = StringCalculator.add("");
-      assert(result === 0);
+      expect(result).to.equal(0);
     });
   });
 
   describe("when a number is passed in", function() {
     it("returns the number", function() {
       var result = StringCalculator.add("2");
-      assert(result === 2);
+      expect(result).to.equal(2);
     });
   });
 
   describe("when string is passed in", function() {
     it("returns NaN", function() {
       var result = StringCalculator.add("a");
-      assert(isNaN(result));
+      console.log(result);
+      expect(result).to.not.be.undefined;
+      expect(result).to.not.be.null;
+      expect(result).to.satisfy(function NaN(num) { return isNaN(num); },
+                                'expected NaN');
     });
   });
 
   describe("when '1,2' is passed in", function() {
     it("returns 3", function() {
       var result = StringCalculator.add("1,2");
-      assert(result === 3);
+      expect(result).to.equal(3);
     });
   });
 });
