152ff75f52014a820085c80f163be7352372cf43
Anders Janmyr <anders@janmyr.com>
2013-12-16 08:59:05 +0100

    Added lab_spec

--- javascript-lang-course/lab/lab.js
+++ javascript-lang-course/lab/lab.js
@@ -1,6 +1,15 @@
-(function() {
+
+var lab = {};
+
+(function(exports) {
     'use strict';
 
     console.log('Write your code inside this');
 
-}());
+    function sane() {
+        return 'Sane';
+    }
+
+    exports.sane = sane;
+
+}(lab));
---
+++ javascript-lang-course/lab/lab_spec.js
@@ -0,0 +1,8 @@
+describe("lab", function() {
+
+  describe("Sanity check", function() {
+    it("Checks the return value of sane", function() {
+      expect(lab.sane()).to.equal('Sane');
+    });
+  });
+});
